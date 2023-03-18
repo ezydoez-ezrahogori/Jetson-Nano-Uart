a26296171dee015ed3ff3c6534eb151ea06efbe5
Bukiezrah <79102300+ezydoez-ezrahogori@users.noreply.github.com>
2023-03-18 17:06:08 +0300

    Update Uart.h

---
+++ Uart.h
@@ -0,0 +1,26 @@
+#ifndef _UART_
+#define UART_H
+
+//define constants
+
+const char *uart_target = "/dev/ttyTHS1";
+#define NSERIAL_CHAR   256
+#define VMINIX         1
+#DEFINE BAUDRATE       B115200
+
+class Uart {
+private:
+  /* data */
+  int fid;
+public:
+  unsigned char serial_message[NSERIAL_CHAR];
+
+
+  Uart ();
+  void sendUart(unsigned char *msg);
+  bool sendUart_fb(unsigned char *msg);
+  void readUart();
+  void closeUart();
+
+};
+#endif
